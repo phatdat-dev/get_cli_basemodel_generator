2ca9e0e14e6fea3972c23ccef3811ebee9f9cac7
wwwwww3q <phatdatfbi@gmail.com>
2022-10-20 11:03:42 +0700

    ignore constant_identifier_names

--- lib/samples/impl/generate_locales.dart
+++ lib/samples/impl/generate_locales.dart
@@ -12,7 +12,7 @@ class GenerateLocalesSample extends Sample {
   String get content => '''
 // DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
 
-// ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: lines_longer_than_80_chars, constant_identifier_names
 // ignore: avoid_classes_with_only_static_members
 class AppTranslation {
 
